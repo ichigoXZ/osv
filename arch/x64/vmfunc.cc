205960720cbe22edddeeb5cd279a82ec94a87a52
btan <atathunder@gmail.com>
2020-01-04 19:42:43 +0800

    trampoline

--- arch/x64/vmfunc.cc
+++ arch/x64/vmfunc.cc
@@ -2,6 +2,29 @@
 
 long vmfunc(long p1, long p2, long p3, long n, long p4, long p5)
 {
+    asm("movq %0, %%rcx;"
+        : 
+        : "r"(n)
+        : "rcx");
     long r = trampoline();
+    if (n == 0)
+        install();
     return r;
+}
+
+void install()
+{
+    uint64_t target_rsp = 0xffff800002000000;
+    // target_rsp -= ((uint64_t)1 << 0x20);
+    __asm__ volatile("mov %rsp, %r10;");
+    for (int i = 1; i <= 1024; i++)
+    {
+        __asm__ volatile("movq %0, %%rsp;"
+                         "push %%rsp;"
+                         :
+                         : "r"(target_rsp)
+                         : "rsp");
+        target_rsp -= (1 << 0x12);
+    }
+    __asm__ volatile("mov %r10, %rsp;");
 }
--- include/osv/vmfunc.hh
+++ include/osv/vmfunc.hh
@@ -1,9 +1,11 @@
 #ifndef OSV_VMFUNC_HH_
 #define OSV_VMFUNC_HH_
+#include <stdint.h>
 
 extern "C" long trampoline(void);
 
 // rdi, rsi, rdx, rcx, r8, r9
 long vmfunc(long p1, long p2, long p3, long n, long p4, long p5);
+void install();
 
 #endif
